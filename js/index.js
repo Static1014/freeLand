849f88e1bf66fd678a808458ff0e058d2b9b4fd1
Static1014 <420048248@qq.com>
2020-07-24 17:30:02 +0800

    测试页

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
-document.getElementById("sub_free").innerHTML = "替换";
+$(function () {
+    // $("#h_title").html("jquery test");
+    // $("#h_title").addClass("blue center");
+    // $("#content_box").css("height", window.screen.availHeight / 2);
+    // $("#content_box").css("background-image", "url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595500028785&di=0cf22a478d30ebded2b409ca8dc560f0&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2017-11-29%2F5a1e22998f77f.jpg)");
+    // $("#content_box").css("background-size", window.screen.availWidth + " " + window.screen.availHeight);
+    // $("#img_box").css("background-image", "url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595500028785&di=0cf22a478d30ebded2b409ca8dc560f0&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2017-11-29%2F5a1e22998f77f.jpg)");
+    // $("#img_box").css("width", 600);
+    // $("#img_box").css("height", 360);
+    // $("#img_box").css("background-size", "100% 100%");
+    // $("#img_box").css("background-repeat", "no-repeat");
+    // $("#img_box").css("background-attachment", "fixed");
 
-function clickContactMe() {
-    console.info("....");
-    window.open("contact.html");
-}
+    // console.info($("#content_box").css("height") + "  -  " + $("#content_box").css("background-color"));
+
+    // $("#btn_contact").click(function () {
+    //     // window.open("contact.html");
+    //     $(this).val("点我干嘛");
+    // });
+    //
+    // console.info("document.body.clientWidth :" + document.body.clientWidth + " - " + document.body.clientHeight);
+    // console.info("document.body.offsetWidth :" + document.body.offsetWidth + " - " + document.body.offsetHeight);
+    // console.info("document.body.scrollWidth :" + document.body.scrollWidth + " - " + document.body.scrollHeight);
+    // console.info("document.body.scrollLeft :" + document.body.scrollLeft + " - " + document.body.scrollTop);
+    // console.info("window.screenLeft :" + window.screenLeft + " - " + window.screenTop);
+    // console.info("window.screen.width :" + window.screen.width + " - " + window.screen.height);
+    // console.info("window.screen.availWidth :" + window.screen.availWidth + " - " + window.screen.availHeight);
+    // console.info("window.devicePixelRatio :" + window.devicePixelRatio);
+
+});
