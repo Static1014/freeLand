be5c5d2afe0d6a972c792cb694e3a66b6fcd5a2e
Static1014 <420048248@qq.com>
2020-07-24 17:32:14 +0800

    测试页

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function () {
     // $("#h_title").html("jquery test");
-    // $("#h_title").addClass("blue center");
+    $("#h_title").addClass("blue center");
     // $("#content_box").css("height", window.screen.availHeight / 2);
     // $("#content_box").css("background-image", "url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595500028785&di=0cf22a478d30ebded2b409ca8dc560f0&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2017-11-29%2F5a1e22998f77f.jpg)");
     // $("#content_box").css("background-size", window.screen.availWidth + " " + window.screen.availHeight);
