a42da9c10b8ae3431cbad768659fbc2980f7acfe
Static1014 <420048248@qq.com>
2020-07-28 17:13:28 +0800

    前端初学

---
+++ js/jike/index.js
@@ -0,0 +1,52 @@
+$(function () {
+    document.title = "《极客学院》Web前段开发教学 - 第一部分：H5+CSS+JS";
+
+    var BASE_VIDEO_URL = "https://www.bilibili.com/video/BV1Mx411m7fd?p=";
+    var menuList = [
+        "观看视频前必读",
+        "1.1.1. 走进前段工程师的世界",
+
+        "2.1.1. HTML5开发前的准备",
+        "2.1.2. 开发前的准备 - 快捷键",
+
+        "2.2.1. HTML5简介",
+        "2.2.2. HTML5集成开发环境搭建",
+        "2.2.3. HTML5基础详解",
+
+        "2.3.1. HTML5元素简介及使用方法",
+        "2.3.2. HTML5属性使用方法",
+        "2.3.3. HTML5格式化及使用",
+
+        "2.4.1. HTML5样式的使用",
+        "2.4.2. HTML5链接属性及使用",
+        "2.4.2. HTML5表格使用",
+    ];
+
+    /**
+     * 创建目录
+     */
+    function createMenu() {
+        var menu = $("<ul></ul>");
+
+        for (let i = 0; i < menuList.length; i++) {
+            let title = "P" + (i + 1) + " " + menuList[i];
+
+            let li = $("<li></li>");
+            if (isExistFile("p" + (i + 1) + ".html")) {
+                let a = $("<a>" + title + "</a>");
+                a.attr("href", encodeURI("p" + (i + 1) + ".html?title=" + title + "&video=" + BASE_VIDEO_URL + (i + 1)));
+                a.attr("target", "_blank");
+                li.append(a);
+            } else {
+                li.append(title);
+            }
+
+
+            menu.append(li);
+        }
+
+        $("body").append(menu);
+    }
+
+    createMenu();
+});
---
+++ js/jike/jikeUtils.js
@@ -0,0 +1,71 @@
+/**
+ * 添加课程title
+ */
+function addHeader() {
+    let title = getUrlParam('title');
+    let videoUrl = getUrlParam('video');
+
+    let header = $("<div></div>");
+    header.id = 'header';
+    header.css('width', '100%');
+
+    let backImg = $("<img>");
+    backImg.attr({
+        "id": "top",
+        "margin-right": "10px",
+        "width": "32px",
+        "title": "返回上一页",
+        "src": "../../res/images/back.png"
+    });
+    backImg.click(function () {
+        goBack();
+    });
+
+    let h1 = $("<h1></h1>");
+    h1.append(title);
+    h1.css({
+        "text-align": "center",
+        "margin-left": "auto",
+        "margin-right": "auto",
+        "width": (getBodySize().width - 84) + "px"
+    });
+
+    let tDiv = $("<div></div>");
+    tDiv.append(backImg);
+    tDiv.append(h1);
+    header.append(tDiv);
+
+    let subDiv = $("<div></div>");
+    subDiv.css({
+        "float": "right",
+        "text-align": "center",
+        "margin": "0 10px 10px 0",
+        "width": "100%"
+    });
+
+    subDiv.append("学习时间：");
+    subDiv.append("2020.07.28&nbsp;&nbsp;&nbsp;&nbsp;");
+
+    subDiv.append("学习来源：");
+    let videoLink = $("<a>B站视频链接</a>");
+    videoLink.attr("href", videoUrl);
+    videoLink.attr("target", "_blank");
+    videoLink.css("color", "darkturquoise");
+    subDiv.append(videoLink);
+
+    subDiv.append("，侵权请");
+
+    let delLink = $("<a>联系删除</a>");
+    delLink.attr("href", "../../contact.html");
+    delLink.attr("target", "_blank");
+    delLink.css("color", "orangered");
+    subDiv.append(delLink);
+
+    header.append(subDiv);
+    header.append("<hr/>");
+    header.append("<h2>学习内容：</h2>");
+
+    $(document.body).prepend(header);
+
+    document.title = title;
+}
